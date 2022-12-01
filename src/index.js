b0def0bf3a56bd974cf4ef3def5a650041d05d1b
Osorabrian <osorabrian@gmail.com>
2022-12-01 12:55:38 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+  form = document.querySelector('form')
+  form.addEventListener('submit', addToDo)
+
+  function addToDo(event){
+            event.preventDefault()
+            const inputValue = form.querySelector('#new-task-description').value
+            const p = document.createElement('p')
+            const btn = document.createElement('button')
+            btn.innerText = 'x'
+            p.innerText = `${inputValue} `
+            form.appendChild(p).appendChild(btn)
+            btn.addEventListener('click', () => {
+              p.remove()
+            })
+            form.reset()
+  }
+
+
+     
 });
